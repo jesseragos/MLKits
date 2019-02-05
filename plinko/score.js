503cb3bb2dc5185043cdaa5cae709c1ed98e7660
Jesse Ragos <jkiel.r4gos@gmail.com>
2019-02-05 16:56:59 +0800

    Implement accuracy (lodash simplified)

--- plinko/score.js
+++ plinko/score.js
@@ -13,16 +13,25 @@ function runAnalysis() {
   // For accuracy testing
   const [testSet, trainingSet] = splitData(outputs, testSetSize);
 
-  let numberCorrect = 0;
-  for(let i=0; i < testSet.length; i++) {
-    const bucket = knn(trainingSet, testSet[i][0]);
-    // console.log("Bucket", bucket, testSet[i][3]);
-    if(bucket === testSet[i][3]) {
-      numberCorrect++;
-    }
-  }
+  // let numberCorrect = 0;
+  // for(let i=0; i < testSet.length; i++) {
+  //   const bucket = knn(trainingSet, testSet[i][0]);
+  //   // console.log("Bucket", bucket, testSet[i][3]);
+  //   if(bucket === testSet[i][3]) {
+  //     numberCorrect++;
+  //   }
+  // }
 
-  console.log('Accuracy: ', numberCorrect / testSetSize);
+  // console.log('Accuracy: ', numberCorrect / testSetSize);
+
+  // Simplified Lodash methods instead of commented code above
+  const accuracy = _.chain(testSet)
+                      .filter(testPoint => knn(trainingSet, testPoint[0]) === testPoint[3])
+                      .size()
+                      .divide(testSetSize)
+                      .value();
+
+  console.log('Accuracy: ', accuracy);
 }
 
 // KNN algo implementation
