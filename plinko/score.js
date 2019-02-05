b12f946760b988b47aa3a2ab4505bcfaad388bcf
Jesse Ragos <jkiel.r4gos@gmail.com>
2019-02-05 14:16:32 +0800

    Implement accuracy method

--- plinko/score.js
+++ plinko/score.js
@@ -1,6 +1,7 @@
 const outputs = [];
 const predictionPoint = 300;
 const k = 3;    // k variable
+const testSetSize = 10; //  10 random separate points
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
@@ -9,24 +10,49 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
+  // For accuracy testing
+  const [testSet, trainingSet] = splitData(outputs, testSetSize);
 
-  // KNN algo implementation
-  const bucket =  _.chain(outputs)                
-                    .map(row => [distance(row[0]), row[3]]) // [ [ 290, 1 ], [ 100, 4 ], [ 50, 4 ], [ 300, 5 ] ]
-                    .sortBy(row => row[0])  //  [ [ 50, 4 ], [ 100, 4 ], [ 290, 1 ], [ 300, 5 ] ]
-                    .slice(0, k)    //  [ [ 50, 4 ], [ 100, 4 ], [ 290, 1 ] ]
-                    .countBy(row => row[1]) //  { '1': 1, '4': 2 }
-                    .toPairs(row => row[1]) //  [ [ '1', 1 ], [ '4', 2 ] ]
-                    .sortBy(row => row[1])  //  [ [ '1', 1 ], [ '4', 2 ] ]
-                    .last()     //  [ '4', 2 ]
-                    .first()    // '4'
-                    .parseInt() //  4 
-                    .value();
+  let numberCorrect = 0;
+  for(let i=0; i < testSet.length; i++) {
+    const bucket = knn(trainingSet, testSet[i][0]);
+    // console.log("Bucket", bucket, testSet[i][3]);
+    if(bucket === testSet[i][3]) {
+      numberCorrect++;
+    }
+  }
 
-    console.log("Your point wlil probably fall into ", bucket);
+  console.log('Accuracy: ', numberCorrect / testSetSize);
 }
 
-function distance(point) {
-  return Math.abs(point - predictionPoint);
+// KNN algo implementation
+function knn(data, point) {
+  return _.chain(data)                
+            .map(row => [distance(row[0], point), row[3]]) // [ [ 290, 1 ], [ 100, 4 ], [ 50, 4 ], [ 300, 5 ] ]
+            .sortBy(row => row[0])  //  [ [ 50, 4 ], [ 100, 4 ], [ 290, 1 ], [ 300, 5 ] ]
+            .slice(0, k)    //  [ [ 50, 4 ], [ 100, 4 ], [ 290, 1 ] ]
+            .countBy(row => row[1]) //  { '1': 1, '4': 2 }
+            .toPairs(row => row[1]) //  [ [ '1', 1 ], [ '4', 2 ] ]
+            .sortBy(row => row[1])  //  [ [ '1', 1 ], [ '4', 2 ] ]
+            .last()     //  [ '4', 2 ]
+            .first()    // '4'
+            .parseInt() //  4 
+            .value();
+}
+
+// Get distance from prediction point
+function distance(pointA, pointB) {
+  return Math.abs(pointA - pointB);
+}
+
+// Split data for test and training set
+function splitData(data, testCount) {
+  // Shuffle data to prevent bias
+  const shuffled = _.shuffle(data);
+
+  // Slice data for test set and training set
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+
+  return [testSet, trainingSet];
 }
